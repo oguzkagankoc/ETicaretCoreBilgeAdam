957aeec81bae7ea05aaa7bb3a0cb68f2af168805
Çağıl Alsaç <cagilalsac@outlook.com>
2022-02-06 14:10:21 +0300

    v1

---
+++ AppCore/Business/Services/Bases/IService.cs
@@ -0,0 +1,15 @@
+﻿using AppCore.DataAccess.Bases.EntityFramework;
+using AppCore.Records.Bases;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCore.Business.Services.Bases
+{
+    public interface IService<TModel, TEntity, TDbContext> : IDisposable where TModel : RecordBase, new() where TEntity : RecordBase, new() where TDbContext : DbContext, new()
+    {
+        RepositoryBase<TEntity, TDbContext> Repository { get; set; }
+        IQueryable<TModel> Query();
+        bool Add(TModel model);
+        bool Update(TModel model);
+        bool Delete(int id);
+    }
+}
