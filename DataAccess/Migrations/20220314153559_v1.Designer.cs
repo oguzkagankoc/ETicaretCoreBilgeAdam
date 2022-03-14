dafd04b3f0f184789de0d5ce8167a22727304e5b
Çağıl Alsaç <cagilalsac@outlook.com>
2022-03-14 18:46:04 +0300

    v1

--- DataAccess/Migrations/20220314153559_v1.Designer.cs
+++ DataAccess/Migrations/20220314153559_v1.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(ETicaretContext))]
-    [Migration("20220314134101_v1")]
+    [Migration("20220314153559_v1")]
     partial class v1
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
